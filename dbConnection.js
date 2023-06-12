2ec0ab1c6d4f9d9aa13e9672a0ca06df36b79157
Michael Reid <mreid@xpanxion.com>
2023-06-12 11:22:54 -0400

    Update

--- dbConnection.js
+++ dbConnection.js
@@ -1,3 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
+exports.promisePool = mysql.createPool(
+    {
+        host:      'localhost',
+        user:       'root',
+        password:   'mR2944360$',
+        database:   'sql-primer' 
+    }).promise();
 // create pool or connection
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const cors = require('cors')
 const path = require('path');
 const PORT = 8080;
-
-
 const app = express();
+const { promisePool } = require('./dbConnection.js');
 // parses JSON from incoming request
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const corsOptions = {
+  origin: ["http:// localhost:3000"], 
+  optionSuccessStatus:200
+};
 
 // Do not edit
 const options = {
@@ -17,34 +24,43 @@ const options = {
 
 // #3 helper function 'getColor`
 const getColor = (fruit) => {
+  const color = options[fruit];
+  return color; 
 }
 
 // #1 serve the colors.html page when /colors is visited
 // DO NOT USE express.static
-app.get('/colors', () => {
-  
+app.get('/colors', cors(corsOptions), (req, res) => {
+  const ABSOLUTE_PATH = path.join(__dirname, './client/colors.html');
+  res.sendFile(ABSOLUTE_PATH);
 });
 
 // #2 & #4 handle POST requests to /colors
-app.post('/colors', () => {
-  
+app.post('/colors', cors(corsOptions), async (req, res) => {
+  const { fruit } = req.body;
+  const color = getColor(fruit);
+  res.json(color);
 });
 
 // #6 serve styles.css - DO NOT use express.static()
-app.get('/styles.css', () => {
-
+app.get('/styles.css', cors(corsOptions), (req, res) => {
+  const ABSOLUTE_PATH = path.join(__dirname, './client/styles.css');
+  res.sendFile(ABSOLUTE_PATH)
 });
 
 // #5 Update functionality to database
-app.put('/colors/:id/:fruit', () => {
-
+app.put('/colors/:id/:fruit',cors(corsOptions), async (req, res) => {
+  const { id, fruit } = req.params;
+  const color = getColor(fruit);
+  const [rows] = await promisePool.execute(`UPDATE cars SET color = ?' WHERE car_id = ?`, [color, id]);
+  res.send(rows);
 });
 
 // #7 unknown routes - 404 handler
 // research what route to serve this for
-app.get('', () => {
-  
-})
+app.get('/*', cors(corsOptions), (req, res) => {
+  res.send('404');
+});
 
 // Global error handling middleware
 // You can leave this alone
